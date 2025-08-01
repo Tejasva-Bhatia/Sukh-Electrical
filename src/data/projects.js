484af8a4f619905f2f1782ed49804807df3d609f
Tejasva-Bhatia <bhatiatejasva15@gmail.com>
2025-08-01 01:05:48 -0500

    projects updated

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,7 +3,7 @@ const projects = [
     id: 1,
     img: '/images/project-gallery/outdoor-light.jpg',
     largeimg: '/images/project-gallery/outdoor-light-L.jpg',
-    heading: "Outdoor Lighting",
+    heading: "🏡 Outdoor Lighting",
     desc: "Outdoor lighting installed for exterior enhancement project",
     portrait: false
   },
@@ -11,23 +11,23 @@ const projects = [
     id: 2,
     img: '/images/project-gallery/smoke-detector.jpg',
     largeimg: '/images/project-gallery/smoke-detector.jpg',
-    heading: "Smoke Detector Replacement",
+    heading: "🔥 Smoke Detector Replacement",
     desc: "Swapped out old unit for updated fire safety device.",
     portrait: false
   },
   {
     id: 3,
-    img: '/images/project-gallery/security-camera-S.jpg',
-    largeimg: '/images/project-gallery/security-camera-L.jpg',
-    heading: "Security Camera",
-    desc: "Outdoor WIFI security camera",
-    portrait: false
+    img: '/images/project-gallery/chandelier.jpg',
+    largeimg: '/images/project-gallery/chandelier.jpg',
+    heading: "💎 Grand Chandelier Installation",
+    desc: "Installed statement chandelier to elevate interior with elegance and ambient lighting.",
+    portrait: true
   },
   {
     id: 4,
     img: '/images/project-gallery/fan.jpg',
     largeimg: '/images/project-gallery/fan.jpg',
-    heading: "Ceiling Fan Installation",
+    heading: "🌬️ Ceiling Fan Installation",
     desc: "Mounted and wired ceiling fan for improved air circulation and energy efficiency.",
     portrait: false
   },
@@ -35,15 +35,15 @@ const projects = [
     id: 5,
     img: '/images/project-gallery/light-fitting-S.jpg',
     largeimg: '/images/project-gallery/light-fitting-S.jpg',
-    heading: "LED Lights",
-    desc: "Suspended LED light fitting installed in a kitchen",
+    heading: "🔆 LED Lights",
+    desc: "Suspended LED light fitting installed in a living room",
     portrait: true
   },
   {
     id: 6,
     img: '/images/project-gallery/Oven-RCD.jpg',
     largeimg: '/images/project-gallery/Oven-RCD.jpg',
-    heading: "Oven & RCD Installatio",
+    heading: "📟 Oven & RCD Installatio",
     desc: "Oven and RCD installed for kitchen upgrade project.",
     portrait: true
   },
@@ -51,7 +51,7 @@ const projects = [
     id: 7,
     img: '/images/project-gallery/tesla-wall-charger.jpg',
     largeimg: '/images/project-gallery/tesla-wall-charger.jpg',
-    heading: "Tesla EV Charger Install",
+    heading: "⚡ Tesla EV Charger Install",
     desc: "Installed 32A Tesla Wall Charger for home EV charging solution.",
     portrait: false
   },
@@ -59,23 +59,23 @@ const projects = [
     id: 8,
     img: '/images/project-gallery/power-point.jpg',
     largeimg: '/images/project-gallery/power-point.jpg',
-    heading: "Outdoor Weatherproof Power Point Installation",
+    heading: "🌦️ Outdoor Weatherproof Power Point Installation",
     desc: "Mounted weatherproof outlet for safe exterior use.",
     portrait: false
   },
   {
     id: 9,
-    img: '/images/project-gallery/outdoor-lighting-S.jpg',
-    largeimg: '/images/project-gallery/outdoor-lighting-L.jpg',
-    heading: "Outdoor Lighting",
+    img: '/images/project-gallery/deck.jpg',
+    largeimg: '/images/project-gallery/deck.jpg',
+    heading: "🔦 Outdoor Lighting",
     desc: "Outdoor lighting and power installation for a decking project",
-    portrait: true
+    portrait: false
   },
   {
     id: 10,
     img: '/images/project-gallery/suspended-led-s.jpg',
     largeimg: '/images/project-gallery/suspended-led.jpg',
-    heading: "LED Pendant Light Installation",
+    heading: "✨ LED Pendant Light Installation",
     desc: "Delivered stylish kitchen lighting upgrade with suspended LED feature.",
     portrait: true
   },
@@ -83,7 +83,7 @@ const projects = [
     id: 11,
     img: '/images/project-gallery/exhaust-fan.jpg',
     largeimg: '/images/project-gallery/exhaust-fan-l.jpg',
-    heading: "Bathroom Exhaust Fan Installation",
+    heading: "🚿 Bathroom Exhaust Fan Installation",
     desc: "Installed ventilation fan to improve airflow and reduce moisture buildup.",
     portrait: false
   },
@@ -91,8 +91,40 @@ const projects = [
     id: 12,
     img: '/images/project-gallery/ac.jpg',
     largeimg: '/images/project-gallery/ac.jpg',
-    heading: "Air Conditioner Installation",
+    heading: "❄️ Air Conditioner Installation",
     desc: "Installed split AC system for efficient home cooling upgrade.",
+    portrait: false
+  },
+  {
+    id: 13,
+    img: '/images/project-gallery/44.jpg',
+    largeimg: '/images/project-gallery/44.jpg',
+    heading: "🔴 Dining Room Pendant Lighting Installation",
+    desc: "Red and white suspended dining lights installed for mood and table focus.",
+    portrait: false
+  },
+  {
+    id: 14,
+    img: '/images/project-gallery/22.jpg',
+    largeimg: '/images/project-gallery/22.jpg',
+    heading: "💨 Stylish Ceiling Fans with Lights Throughout the Home",
+    desc: " Bright, breezy, modern fans installed across rooms for comfort and style.",
+    portrait: false
+  },
+  {
+    id: 15,
+    img: '/images/project-gallery/1.jpg',
+    largeimg: '/images/project-gallery/1.jpg',
+    heading: "💡✨ Oversized Modern Ceiling Light for Sleek Kitchen Ambiance",
+    desc: "Bold ceiling fixture adds style, brightness, and modern flair to your kitchen.",
+    portrait: true
+  },
+  {
+    id: 16,
+    img: '/images/project-gallery/28.jpg',
+    largeimg: '/images/project-gallery/28.jpg',
+    heading: "🍽️ Modern Kitchen Island with Pendant Lighting & Bar Seating",
+    desc: "Sleek Kitchen Island with Pendant Lights and Bar Seating",
     portrait: false
   },
 ];
